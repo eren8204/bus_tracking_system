0237ef2d23c80d1234a0f3ce17416ef5f891b5cc
SANJAY <VARTIKSAXENA123@GMAIL.COM>
2024-12-11 12:33:12 +0530

    update/arsh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ realm {
     isSyncEnabled = true
 }
 dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
     implementation ("com.airbnb.android:lottie:6.0.0")
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
     implementation ("androidx.webkit:webkit:1.8.0")
--- app/src/main/java/com/example/hackathon/firstactivity.java
+++ app/src/main/java/com/example/hackathon/firstactivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.bumptech.glide.Glide;
 
 public class firstactivity extends AppCompatActivity {
 
@@ -21,39 +23,42 @@ public class firstactivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_firstactivity);
 
+        ImageView gifImageView = findViewById(R.id.gifImageView);
+        Glide.with(this).asGif().load(R.raw.bus).into(gifImageView);
 
         new Handler().postDelayed(() -> {
-            // Navigate to the main activity
+
             Intent intent = new Intent(firstactivity.this, splashActivity.class);
             startActivity(intent);
             finish();
-        }, 7500);
-        LottieAnimationView lottieAnimationView = findViewById(R.id.lottieAnimationView);
+        }, 4500);
 
-
-        lottieAnimationView.playAnimation();
-
-
-        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
+//        LottieAnimationView lottieAnimationView = findViewById(R.id.lottieAnimationView);
+//
+//
+//        lottieAnimationView.playAnimation();
+//
+//
+//        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
     }
 }
